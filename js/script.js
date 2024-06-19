46c8c36b496064a8ca479e965ffb813852ccffc9
anjalirathor622 <anjalirathor622@gmail.com>
2024-06-19 18:52:50 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // console.log('hello')
+//get element
 let addbtn = document.getElementById("addbtn");
 showNotes();
 
+//addeventlistener to handle add notes button
 addbtn.addEventListener("click",function(){
 
     let addtext = document.getElementById("addtext");
@@ -10,17 +12,17 @@ addbtn.addEventListener("click",function(){
     if(storedNotes == null){
         allNotes = [];
     }else{
-        allNotes = JSON.parse(storedNotes);
+        allNotes = JSON.parse(storedNotes);  // convertnotes to json arraylist
 
     }
-    allNotes.push(addtext.value);
+    allNotes.push(addtext.value);           //push all notes to the array
 
-    localStorage.setItem("notes", JSON.stringify(allNotes));
+    localStorage.setItem("notes", JSON.stringify(allNotes));   //set the notes and convert them to json string
     addtext.value = "";
     showNotes();
 })
 
-//function  definition
+//function to shhow  notes 
 function showNotes() {
     let storedNotes = localStorage.getItem("notes");
     if(storedNotes==null) {
@@ -29,7 +31,7 @@ function showNotes() {
         allNotes=JSON.parse(storedNotes);
     }
     let html = "";
-    allNotes.forEach(function(element,index){
+    allNotes.forEach(function(element,index){     //looping on notes and generating html for each note
         html += `<div class="card m-2" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">MyNote ${index+1}</h5>
@@ -38,8 +40,10 @@ function showNotes() {
             </div>
           </div>`
     })
+
     let notesTextElem = document.getElementById("notes");
-    if(allNotes.length!=0){
+    //checking for notes and seting the inner html
+    if(allNotes.length!=0){        
         notesTextElem.innerHTML = html;
     }else{
         notesTextElem.innerHTML = "There is nothing to show,let's create your first note;it will make things documented.";
